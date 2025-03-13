382c7e15ed1dc6a81674da450bd3d7326c4e9941
Bryson Gichuhi <gichuhibryson09@gmail.com>
2025-03-13 14:21:56 +0300

    commit

--- Rems/src/Pages/Users/Settings.jsx
+++ Rems/src/Pages/Users/Settings.jsx
@@ -1,9 +1,12 @@
 //import React from 'react'
 import { IoMdSettings } from 'react-icons/io'
+import Navbar from '../Navbar'
 
-const Settings = ({sidebar, setSidebarToggle}) => {
+const Settings = ({sidebarToggle, setSidebarToggle}) => {
   return (
     <div className={`${sidebarToggle? "" : "ml-48"}`}>
+      <Navbar sidebarToggle={sidebarToggle}
+      setSidebarToggle={setSidebarToggle}/>
         <div>
         <IoMdSettings />
         </div>
