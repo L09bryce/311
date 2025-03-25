044a419ea334185f18c55d5422fd0f4d1494084a
Bryson Gichuhi <gichuhibryson09@gmail.com>
2025-03-25 23:18:07 +0300

    login page guess done

--- Rems/src/LoginPage/Login.jsx
+++ Rems/src/LoginPage/Login.jsx
@@ -1,23 +1,41 @@
 //import React from 'react'
 //import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react';
 import './Login.css'
 
 const Login = () => {
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword]= useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/login", { username, password });
+      setUser(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
    
   return (
-    <div className='login  p-40 flex'>
+    <div className='login  p-30 flex'>
         <div>
-            <h1>PRADORA</h1>
+            <h1 className='pt-15 ml-30'>PRADORA</h1>
           </div>
 
           <div className="credentials ml-50">
-            <form action="PUT">
-              <input type='text' placeholder='EnterUsername or Email' name='username' className='border-2 border-r-15 w-64'/>
-              <input type='password' name='password' placeholder='Password' className='mt-20 border-2 w-64'/><br />
-              <button className='text-blue-600 btn2'>or forgot Password?</button>
+            <form onSubmit={handleSubmit}>
+              <input type='text' placeholder='EnterUsername or Email' name='username' className='p-0.5 mx-4 border-1 w-48'
+              onChange={(e) => setUsername(e.target.value)}
+              />
+              <input type='password' 
+              name='password' 
+              placeholder='Password' 
+              className='p-0.5 mx-4 mt-20 border-1 w-48'
+              onChange={(e) => setPassword(e.target.value)}/><br />
               <button className='btn1 text-white'>Login</button><br />
-              
-              <button className='btn3 text-green-600'>Create Account</button>
             </form>
           </div>
 
--- Server/index.js
+++ Server/index.js
@@ -25,6 +25,25 @@ const users = [
         password: "qlkj4276#"
     }
 ]
+//refresh tokens
+app.post("/api/refresh", (req, res) => {
+    //take the refresh tokens from the user
+    const refreshToken = req.body.token
+
+    //check whether token is valid ot invalid
+    if(!refreshToken) return res.status(401).json("You are not authenticated")
+    if(!refreshTokens.includes(refreshToken)) return res.status(403).json("Refreshtoken is invalid!")
+
+    //if everything is okay
+})
+const generateAccessToken = (user) => {
+    return jwt.sign({id: user.id}, "mySecretKey",
+    {expiresIn: "15m"}
+)};
+const generateRefreshToken = (user) => {
+    return jwt.sign({id: user.id}, "myRefreshSecretKey",
+)};
+
 //login route
 app.post("/api/login", (req,res) => {
     const {username, password} =req.body;
@@ -32,10 +51,40 @@ app.post("/api/login", (req,res) => {
         return u.username === username && u.password === password
     });
     if(user){
-        res.json(user)
+        //Generate accesstokens
+        const accessToken = generateAccessToken(user)
+        const refreshToken = generateRefreshToken(user)
+        refreshTokens.push(refreshToken)
+        res.json({
+            user: user.username,
+            accessToken,
+        })
     }else{
         res.status(400).json("User or Password invalid!")
     }   
+});
+
+const verify = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if(authHeader){
+        const token = authHeader.split(" ")[1];
+
+        jwt.verify(token, "mySecretKey", (err, user) =>{
+            if(err){
+                return res.status(403).json("Token is not valid")
+            }
+            req.user = user;
+            next();
+        });
+    }else{
+        res.status(401).json("You are not authenticated")
+    }
+};
+
+app.post("/api/logout", verify, (req, res) =>{
+    const refreshToken = req.body.token;
+    refreshTokens = refreshTokens.filter(token !== refreshToken);
+    res.status(200).json("Logged out successful")
 });
 
 app.listen(5000, () => console.log("Backend is working!"));
